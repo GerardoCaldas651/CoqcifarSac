0b8541d9175a0f60ece369d68496ead8a30f2e06
Gerardo Gabriel Caldas Díaz <u201512034@upc.edu.pe>
2024-01-18 19:14:10 -0500

    Add Movil
    App Movil by Coqcifar

---
+++ coqcifar_movil/android/app/src/main/kotlin/com/example/coqcifar_movil/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coqcifar_movil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ coqcifar_movil/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.cyan
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+// 38:54
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
